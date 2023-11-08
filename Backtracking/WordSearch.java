2b33734bf245b6afe976383c66db6130ba6132bb
skhattab <ksm73@pitt.edu>
2023-11-08 19:03:49 +0000

    assignment 4 support

--- Backtracking/WordSearch.java
+++ Backtracking/WordSearch.java
@@ -2,57 +2,73 @@ import java.io.File;
 import java.util.Scanner;
 
 public class WordSearch {
-    
+
     private char[][] grid;
     private String wordToFind;
 
-    public static void main(String[] args){
-        if(args.length != 1){
-            System.out.println("Usage: java WordSearch <fileName>");
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java WordSearch <fileName> <word to find>");
         } else {
-            WordSearch ws = new WordSearch(args[0], 10);
-            ws.find("DATASTRUCTURES");
+            WordSearch ws = new WordSearch(args[0]);
+            if (ws.find(args[1])) {
+                System.out.println("Word Found!");
+                System.out.println("===========");
+                ws.printGrid();
+            } else {
+                System.out.println("Word not found!");
+            }
         }
     }
 
-    public WordSearch(String fileName, int size){
-        grid = new char[size][size];
+    public WordSearch(String fileName) {
         Scanner fileScan = null;
-        try{
+        try {
             fileScan = new Scanner(new File(fileName));
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error opening file " + e.getMessage());
         }
-        if(fileScan != null){
+        if (fileScan != null) {
             int row = 0;
-            while(fileScan.hasNextLine()){
-                String input = fileScan.nextLine();
-                for(int i=0; i<size; i++){
-                    grid[row][i] = Character.toLowerCase(
-                            input.charAt(i));
+            int size = Integer.parseInt(fileScan.nextLine());
+            System.out.println("Found a " + size + "x" + size + " puzzle.");
+            grid = new char[size][size];
+
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    char c = fileScan.next().charAt(0);
+                    grid[i][j] = Character.toLowerCase(c);
                 }
-                row++;
             }
+
             fileScan.close();
         }
-        
+
     }
 
-    public boolean find(String word){
+    public boolean find(String word) {
         boolean result = false;
-        wordToFind = word.toUpperCase();
+        wordToFind = word.toLowerCase();
 
-        //call solve
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid.length; j++){
-                if(Character.toUpperCase(grid[i][j]) == wordToFind.charAt(0)) {
-                    StringBuilder temp = new StringBuilder();
-                    temp.append(Character.toUpperCase(grid[i][j]));
+        // for all possible choices for the first decision
+        for (int i = 0; i < grid.length && !result; i++) {
+            for (int j = 0; j < grid.length && !result; j++) {
+                // if matching letter
+                if (grid[i][j] == wordToFind.charAt(0)) {
+                    // mark cell as used
                     grid[i][j] = Character.toUpperCase(grid[i][j]);
-                    if(solve(i, j, temp)){
+                    // if last letter, return true
+                    if (word.length() == 1) {
                         result = true;
                         break;
+                    } else {
+                        // else make a call to solve starting from next decision
+                        if (solve(i, j, word, 1)) {
+                            result = true;
+                            break;
+                        }
                     }
+                    // unmark cell
                     grid[i][j] = Character.toLowerCase(grid[i][j]);
                 }
             }
@@ -60,112 +76,90 @@ public class WordSearch {
         return result;
     }
 
-
-    private boolean solve(int row, int col, StringBuilder current){
+    private boolean solve(int row, int col, String wordToFind, int letterNo) {
         boolean result = false;
-        //for all possible options
-        for(int direction=0; direction<8; direction++){ //8 directions
-            //check if option is valid
+        // for all possible neighbors
+        for (int direction = 0; direction < 8; direction++) { // 8 directions
+            // if inside the grid and hasn’t been used and == word[letter no.] {
             int nextRow = nextRow(row, direction);
             int nextCol = nextCol(col, direction);
-            if(isValid(nextRow, nextCol)){
-                current.append(Character.toUpperCase(grid[nextRow][nextCol]));
-                grid[nextRow][nextCol] = 
-                    Character.toUpperCase(grid[nextRow][nextCol]);
-                if(current.toString().toUpperCase().equals(wordToFind)){
-                    System.out.println("Word Found!");
-                    printGrid();
+            if (isValid(nextRow, nextCol) && wordToFind.charAt(letterNo) == grid[nextRow][nextCol]) {
+                // mark neighbor as used
+                grid[nextRow][nextCol] = Character.toUpperCase(grid[nextRow][nextCol]);
+                // if last letter, print solution and return true
+                if (letterNo == wordToFind.length() - 1) {
                     result = true;
-                   break;
-                }
-                if (isPrefix(current.toString(), wordToFind)){
-                    if(solve(nextRow, nextCol, current)){
+                    break;
+                } else {
+                    // else make a recursive call on neighbor with letterNo + 1
+                    if (solve(nextRow, nextCol, wordToFind, letterNo + 1)) {
                         result = true;
-                       break;
+                        break;
                     }
                 }
-                
-                //delete last letter of current
-                current.deleteCharAt(current.length()-1);
-                grid[nextRow][nextCol] = 
-                    Character.toLowerCase(grid[nextRow][nextCol]);
-
+                // unmark the neighbor
+                grid[nextRow][nextCol] = Character.toLowerCase(grid[nextRow][nextCol]);
             }
         }
         return result;
-
-
     }
 
-    private boolean isValid(int row, int col){
+    private boolean isValid(int row, int col) {
         boolean result = false;
-        if((row >= 0) && (col >= 0) 
-            && (row < grid.length) && (col < grid[0].length) 
-            && Character.isLowerCase(grid[row][col])){
-                result = true;
-            }
+        if ((row >= 0) && (col >= 0)
+                && (row < grid.length) && (col < grid[0].length)
+                && Character.isLowerCase(grid[row][col])) {
+            result = true;
+        }
         return result;
     }
 
-    
-     /*
+    /*
      * directions:
-     *  0: down
-     *  1: down right
-     *  2: right
-     *  3: up right
-     *  4: up
-     *  5: up left
-     *  6: left
-     *  7: down left
+     * 0: down
+     * 1: down right
+     * 2: right
+     * 3: up right
+     * 4: up
+     * 5: up left
+     * 6: left
+     * 7: down left
      */
-    private int nextRow(int row, int direction){
+    private int nextRow(int row, int direction) {
         int result = row;
-        if(direction == 0 || direction == 1 || direction == 7){
+        if (direction == 0 || direction == 1 || direction == 7) {
             result = row + 1;
         }
-        if(direction == 3 || direction == 4 || direction == 5){
+        if (direction == 3 || direction == 4 || direction == 5) {
             result = row - 1;
         }
         return result;
     }
 
-    private int nextCol(int col, int direction){
+    private int nextCol(int col, int direction) {
         int result = col;
-        if(direction == 5 || direction == 6 || direction == 7){
+        if (direction == 5 || direction == 6 || direction == 7) {
             result = col - 1;
         }
-        if(direction == 1 || direction == 2 || direction == 3){
+        if (direction == 1 || direction == 2 || direction == 3) {
             result = col + 1;
         }
         return result;
     }
 
-    private boolean isPrefix(String check, String against){
-        boolean result = true;
-        if(check.length() > against.length()){
-            result = false;
-        } else {
-            for(int i=0; i<check.length(); i++){
-                if(check.charAt(i) != against.charAt(i)){
-                    result = false;
-                    break;
-                }
-            }
-        }       
-
-        return result;
-
-    }
-
-    private void printGrid(){
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid.length; j++){
-                System.out.print(grid[i][j]);
+    private void printGrid() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if(Character.isUpperCase(grid[i][j]))
+                    //print found letters in purple
+                    System.out.print("\u001B[35m" + grid[i][j] + " ");
+                else
+                    //rest in white
+                    System.out.print("\u001B[37m" + grid[i][j] + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
-    
+
 }
