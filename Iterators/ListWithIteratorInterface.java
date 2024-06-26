f645bc63180a9b1308bc1d98854e388bbaad97ab
skhattab <ksm73@pitt.edu>
2024-06-26 10:44:18 +0000

    formatting

--- Iterators/ListWithIteratorInterface.java
+++ Iterators/ListWithIteratorInterface.java
@@ -1,1 +1,12 @@
-import java.util.Iterator;/**   An interface for the ADT list that has an iterator.      @author Frank M. Carrano   @author Timothy M. Henry   @version 4.0*/public interface ListWithIteratorInterface<T> extends ListInterface<T>, Iterable<T>{   public Iterator<T> getIterator();} // end ListWithIteratorInterface
+import java.util.Iterator;
+/**
+   An interface for the ADT list that has an iterator.
+   
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 4.0
+*/
+public interface ListWithIteratorInterface<T> extends ListInterface<T>, Iterable<T>
+{
+   public Iterator<T> getIterator();
+} // end ListWithIteratorInterface
